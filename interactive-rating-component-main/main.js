05791583db05d1ec05750d7b1020c761c8df3acf
Peter Jonathan-Hart <peerodemba@gmail.com>
2023-02-19 00:47:56 +0100

    third commit

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -1,48 +1,122 @@
-var rating = document.getElementById("rating");
-var thanks = document.getElementById("thanks");
-var btn = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const thanks = document.getElementById("thanks");
+const subbtn = document.getElementById("submit");
+const rForm = document.getElementById("rating_form");
+const rvalue = document.getElementsByName("ratings");
+const rScore = document.getElementById("rat_score");
+const val1 = document.getElementById("rating_1");
+const val2 = document.getElementById("rating_2");
+const val3 = document.getElementById("rating_3");
+const val4 = document.getElementById("rating_4");
+const val5 = document.getElementById("rating_5");
 
-var one = document.getElementById("one");
-var two = document.getElementById("two");
-var three = document.getElementById("three");
-var four = document.getElementById("four");
-var five = document.getElementById("five");
-
-function onev(){
-    thanks.childNodes[3].outerHTML = "<p>You selected 1 out of 5</p>"
-    thanks.childNodes[3].setAttribute("class", "board");
+var i;
+function rated(event) {
+  var x = 0;
+  if (val1.hasAttribute("checked")) {
+    i = rvalue[0].value;
+    x = 1;
+    if (x == 1) {
+      rScore.innerHTML = i;
+      rating.setAttribute("class", "hide");
+      thanks.setAttribute("class", "show");
+    }
+  }
+  if (val2.hasAttribute("checked")) {
+    i = rvalue[1].value;
+    x = 1;
+    if (x == 1) {
+      rScore.innerHTML = i;
+      rating.setAttribute("class", "hide");
+      thanks.setAttribute("class", "show");
+    }
+  }
+  if (val3.hasAttribute("checked")) {
+    i = rvalue[2].value;
+    x = 1;
+    if (x == 1) {
+      rScore.innerHTML = i;
+      rating.setAttribute("class", "hide");
+      thanks.setAttribute("class", "show");
+    }
+  }
+  if (val4.hasAttribute("checked")) {
+    i = rvalue[3].value;
+    x = 1;
+    if (x == 1) {
+      rScore.innerHTML = i;
+      rating.setAttribute("class", "hide");
+      thanks.setAttribute("class", "show");
+    }
+  }
+  if (val5.hasAttribute("checked")) {
+    i = rvalue[4].value;
+    x = 1;
+    if (x == 1) {
+      rScore.innerHTML = i;
+      rating.setAttribute("class", "hide");
+      thanks.setAttribute("class", "show");
+    }
+  }
+  if (x == 0) {
+    alert("Please Select A Value On The Form");
+    location.reload();
+  }
 }
 
-function twov(){
-  thanks.childNodes[3].outerHTML = "<p>You selected 2 out of 5</p>"
-  thanks.childNodes[3].setAttribute("class", "board");
+function one() {
+  val1.setAttribute("checked", "true");
+  if (val1.hasAttribute("checked")) {
+    val2.removeAttribute("checked");
+    val3.removeAttribute("checked");
+    val4.removeAttribute("checked");
+    val5.removeAttribute("checked");
+  }
 }
 
-function threev(){
-  thanks.childNodes[3].outerHTML = "<p>You selected 3 out of 5</p>"
-  thanks.childNodes[3].setAttribute("class", "board");
+function two() {
+  val2.setAttribute("checked", "true");
+  if (val2.hasAttribute("checked")) {
+    val1.removeAttribute("checked");
+    val3.removeAttribute("checked");
+    val4.removeAttribute("checked");
+    val5.removeAttribute("checked");
+  }
 }
 
-function fourv(){
-  thanks.childNodes[3].outerHTML = "<p>You selected 4 out of 5</p>"
-  thanks.childNodes[3].setAttribute("class", "board");
+function three() {
+  val3.setAttribute("checked", "true");
+  if (val3.hasAttribute("checked")) {
+    val2.removeAttribute("checked");
+    val1.removeAttribute("checked");
+    val4.removeAttribute("checked");
+    val5.removeAttribute("checked");
+  }
 }
 
-function fivev(){
-  thanks.childNodes[3].outerHTML = "<p>You selected 5 out of 5</p>"
-  thanks.childNodes[3].setAttribute("class", "board");
+function four() {
+  val4.setAttribute("checked", "true");
+  if (val4.hasAttribute("checked")) {
+    val2.removeAttribute("checked");
+    val3.removeAttribute("checked");
+    val1.removeAttribute("checked");
+    val5.removeAttribute("checked");
+  }
 }
 
-function rated() {
-  rating.removeAttribute("class", "show");
-  thanks.removeAttribute("class", "hide");
-  rating.setAttribute("class", "hide");
-  thanks.setAttribute("class", "show");
+function five() {
+  val5.setAttribute("checked", "true");
+  if (val5.hasAttribute("checked")) {
+    val2.removeAttribute("checked");
+    val3.removeAttribute("checked");
+    val4.removeAttribute("checked");
+    val1.removeAttribute("checked");
+  }
 }
 
-btn.addEventListener("click", rated);
-one.addEventListener("click", onev);
-two.addEventListener("click", twov);
-three.addEventListener("click", threev);
-four.addEventListener("click", fourv);
-five.addEventListener("click", fivev);
+val1.addEventListener("click", one);
+val2.addEventListener("click", two);
+val3.addEventListener("click", three);
+val4.addEventListener("click", four);
+val5.addEventListener("click", five);
+subbtn.addEventListener("click", rated);
